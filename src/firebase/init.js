e3dd5efc7acb5ecae5cd728e96264fc80cbc4065
l4l4dev <l4l4dme@gmail.com>
2020-05-26 19:47:25 +0900

    test

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,7 +1,9 @@
 // app's Firebase project configuration
 export const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "deploytest-66fbc.firebaseapp.com",
     databaseURL: "https://deploytest-66fbc.firebaseio.com",
     projectId: "deploytest-66fbc",
     storageBucket: "deploytest-66fbc.appspot.com",
+    appId: process.env.FIREBASE_APP_ID
 };
