bb3bcef45e9af5f4fb74430bf2ec5c2f84aeb31a
l4l4dev <l4l4dme@gmail.com>
2020-05-26 18:32:10 +0900

    add env

---
+++ src/firebase/init.js
@@ -0,0 +1,10 @@
+// app's Firebase project configuration
+export const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "planning-poker-81f0d.firebaseapp.com",
+    databaseURL: "https://planning-poker-81f0d.firebaseio.com",
+    projectId: "planning-poker-81f0d",
+    storageBucket: "planning-poker-81f0d.appspot.com",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
