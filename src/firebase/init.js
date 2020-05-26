4c905026f70c3f5e120cc4b9686d1b9bd8918c3a
l4l4dev <l4l4dme@gmail.com>
2020-05-26 19:02:02 +0900

    set project id

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,10 +1,10 @@
 // app's Firebase project configuration
 export const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "planning-poker-81f0d.firebaseapp.com",
-    databaseURL: "https://planning-poker-81f0d.firebaseio.com",
-    projectId: "planning-poker-81f0d",
-    storageBucket: "planning-poker-81f0d.appspot.com",
+    authDomain: process.env.FIREBASE_PROJECT_ID+".firebaseapp.com",
+    databaseURL: "https://"+process.env.FIREBASE_PROJECT_ID+".firebaseio.com",
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_PROJECT_ID+".appspot.com",
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
 };
